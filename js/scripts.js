ee6fd27d5442ef41aebea87636caaefa98d80697
Manoj Ambati <manoj.k.ambati@gmail.com>
2020-06-28 22:36:59 +0530

    Added site files

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+var callback = function(){
+	$('.item-skills').each(function(){
+		newWidth = $(this).parent().width() * $(this).data('percent');
+		$(this).width(0);
+    $(this).animate({
+        width: newWidth,
+    }, 1000);
+	});
+	$('.icons-red').each(function(){
+		height = $(this).height();
+    $(this).animate({
+        height: 14,
+    }, 2000);
+	});
+};
+$(document).ready(callback);
+
+var resize;
+window.onresize = function() {
+	clearTimeout(resize);
+	resize = setTimeout(function(){
+		callback();
+	}, 100);
+};
+
+var dateString = "1990-05-28";
+$('#age').html(calcAge(dateString)+'YO');
+function calcAge(dateString) {
+  var birthday = +new Date(dateString);
+  return~~ ((Date.now() - birthday) / (31557600000));
+}
